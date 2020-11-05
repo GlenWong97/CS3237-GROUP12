5a1d5b596cc184f29970e3a6b9134f9dd3c5a9ac
permas <permas@gigacover.com>
2020-11-06 01:10:39 +0800

    update frontend

--- frontendV2/src/App.js
+++ frontendV2/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, Fragment } from 'react';
 import './App.css';
+import Layout from './Templates/Layout';
+
 
 var mqtt    = require('mqtt');
 var client  = mqtt.connect('mqtt://test.mosquitto.org:8081', {protocol:'mqtts'});
@@ -12,27 +14,51 @@ function App() {
   client.on('message', function (topic, message) {
     note = message.toString();
     // Updates React state with message 
-    setMesg(note);
+    setMessage(note);
     console.log(note);
     // client.end();
     });
 
   // Sets default React state 
-  const [mesg, setMesg] = useState("nothing heard");
+  const [message, setMessage] = useState("nothing heard");
 
   return (
-    <div className="App">
-    <header className="App-header">
-    <h1>A taste of MQTT in React</h1>
-    <p>The message is: {mesg}</p>
-		<p>
-		<a href="https://www.preciouschicken.com/blog/posts/a-taste-of-mqtt-in-react/"    
-		style={{
-			color: 'white'
-		}}>preciouschicken.com/blog/posts/a-taste-of-mqtt-in-react/</a>
-		</p>
-		</header>
-		</div>
+    <Layout>        
+      <div className="container fixed-bg-3 text-center">
+      
+        <h1 className="text-center display-4 pb-5 pt-5">Sample Outputs</h1>
+        
+        <div className="container-fluid text-center vert-center-sm">
+          <div className="inner-flex-top">
+            <div className="main-30">
+              <img className="md-icon" src="/yes.png" alt="yes" />
+              <h4 className="text-center pb-5 pt-5">User nods head: Yes</h4>
+        
+            </div>
+            <div className="aside-30 vert-center-m">
+              <img className="md-icon" src="/no.png" alt="no" />
+              <h4 className="text-center pb-5 pt-5">User shakes head: No</h4>
+        
+            </div>
+          </div>
+        </div>
+      
+      </div>
+      <br />      
+
+
+
+
+      <div className="container fixed-bg-3 text-center">
+        <h1 className="text-center display-4 pb-5 pt-5">YesNope</h1>
+        {/* <img className="page-icon" src="/logo.png" alt="Logo" /> */}
+        <h1 className="text-center pb-4 pt-5">Message: {message}</h1>
+      </div>
+
+      <br />
+      <br />
+
+    </Layout>
   );
 }
 
